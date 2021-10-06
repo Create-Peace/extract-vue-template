43adc05951a32e3ea5778dff12bb063c41942336
M-Ray <455747495@qq.com>
2021-10-06 15:47:23 +0800

    feat: add Button

---
+++ src/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="button">
+    <button>name</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Button'
+}
+</script>
--- src/index.vue
+++ src/index.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <button @click="handleClick">{{ name }}</button>
+        <Button />
     </div>
 </template>
 <script>
+import Button from './Button.vue'
 const obj = {
     name: 'test',
+    components: {
+        Button
+    },
     data () {
         return {
             name: 'ming'
